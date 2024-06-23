50275d22a8f5e1fff18931009124e3438051bcb6
Younlea.kim <younlea.kim@gmail.com>
2024-06-23 17:44:00 +0900

    Update main.py

--- udp_comm_py/main.py
+++ udp_comm_py/main.py
@@ -4,8 +4,8 @@ def on_receive_callback(command, data, addr):
     print(f"Received message from {addr}: Command: {command}, Data: {data}")
 
 if __name__ == "__main__":
-    local_ip = '127.0.0.1'  # 수신할 IP
-    server_ip = '192.168.1.100'  # 보낼 IP
+    local_ip = '0.0.0.0'  # 수신할 IP
+    server_ip = '192.168.1.41'  # 보낼 IP
     receive_port = 5003  # 수신할 포트
     send_port = 5002  # 보낼 포트
 
